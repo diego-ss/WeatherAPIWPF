0a8a473b1bf6886647e5d89a05889bad16d4fcc1
Diego <diego_sousa.s@outlook.com>
2023-06-28 00:58:50 -0300

    Adicionar arquivos de projeto.

---
+++ mvvmFirstProject/Model/Area.cs
@@ -0,0 +1,9 @@
+﻿namespace mvvmFirstProject.Model
+{
+    public class Area
+    {
+        public string ID { get; set; }
+        public string LocalizedName { get; set; }
+    }
+
+}
---
+++ mvvmFirstProject/Model/Temperature.cs
@@ -0,0 +1,8 @@
+﻿namespace mvvmFirstProject.Model
+{
+    public class Temperature
+    {
+        public Units Metric { get; set; }
+        public Units Imperial { get; set; }
+    }
+}
