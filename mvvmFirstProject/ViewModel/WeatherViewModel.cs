2db151b38e7efde438f69dbdbe46ef4d0c6de0f5
Diego <diego_sousa.s@outlook.com>
2023-06-29 14:41:34 -0300

    Implementação inicial do viewmodel da WeatherWindow com INotifyPropertyChanged

---
+++ mvvmFirstProject/ViewModel/WeatherViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace mvvmFirstProject.ViewModel
+{
+    public class WeatherViewModel : INotifyPropertyChanged
+    {
+        private string query;
+
+        public string Query
+        {
+            get { return query; }
+            set { 
+                query = value;
+                OnPropertyChanged("Query");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
