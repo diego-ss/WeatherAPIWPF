f8b75f9f678d808c162aa58871a729a4bbedf7a2
Diego <diego_sousa.s@outlook.com>
2023-06-29 15:08:51 -0300

    Criando as primeiras propriedades com notificação e realizando binding na view

--- mvvmFirstProject/ViewModel/WeatherViewModel.cs
+++ mvvmFirstProject/ViewModel/WeatherViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using mvvmFirstProject.Model;
+using System.ComponentModel;
 
 namespace mvvmFirstProject.ViewModel
 {
@@ -14,6 +15,30 @@ namespace mvvmFirstProject.ViewModel
                 OnPropertyChanged("Query");
             }
         }
+
+        private CurrentCondition currentCondition;
+
+        public CurrentCondition CurrentCondition
+        {
+            get { return currentCondition; }
+            set { 
+                currentCondition = value;
+                OnPropertyChanged("CurrentCondition");
+            }
+        }
+
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set { 
+                selectedCity = value;
+                OnPropertyChanged("SelectedCity");
+            }
+        }
+
+
 
 
         public event PropertyChangedEventHandler PropertyChanged;
